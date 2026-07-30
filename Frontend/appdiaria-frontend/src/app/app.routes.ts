8aaf943f5c255bfff3b2e529aae29eeb9bf80304
MghAriana <Arianamarilla8@gmail.com>
2026-07-30 16:15:14 -0300

    creacion de estructura de arquitectura modular basada en funcionalidades

--- Frontend/appdiaria-frontend/src/app/app.routes.ts
+++ Frontend/appdiaria-frontend/src/app/app.routes.ts
@@ -1,3 +1,21 @@
 import { Routes } from '@angular/router';
+import { Home } from './features/home/home';
+import { MainLayout } from './layout/main-layout/main-layout';
+import { Dashboard } from './features/dashboard/dashboard';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+ {
+    path: '',
+    component: Home
+  },
+  {
+    path: 'app',
+    component: MainLayout,
+    children: [
+      {
+        path: 'dashboard',
+        component: Dashboard
+      }
+    ]
+  }
+];
